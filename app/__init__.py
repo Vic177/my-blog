d917d915a24113f96764a42065d0dc1761186f49
Vic177 <39151446+Vic177@users.noreply.github.com>
2018-08-03 18:29:21 +0800

    配置类 工厂函数

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLALchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    config[config_name].init_app
+    
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)    
+    db.init_app(app)
+    
+    
+    return app
