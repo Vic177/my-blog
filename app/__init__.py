9a3389b5fa2b4761eb1aecbb9c4ea52e69cb2658
Vic177 <39151446+Vic177@users.noreply.github.com>
2018-08-10 11:00:26 +0800

    创建与注册蓝本

--- app/__init__.py
+++ app/__init__.py
@@ -14,12 +14,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    config[config_name].init_app
     
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)    
     db.init_app(app)
     
+    from .mian import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
     return app
