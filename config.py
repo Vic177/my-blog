88b145d328cfd28484154d1cea3a19601507d2cb
Vic177 <39151446+Vic177@users.noreply.github.com>
2018-08-11 09:02:47 +0800

    创建表，数据库迁移

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SQLALCHEMY_COMMIT_ON_TERDOWN = True
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True 
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Blog]'
     FLASKY_MAIL_SENDER = 'Blog Admin <Blog@example.com>'
     FLASKY_ADMIN = os.environ.get('BLOG_ADMIN')
