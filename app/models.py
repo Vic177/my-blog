bd21553393dabebf7771ba591f4aa9dccdf29d78
Vic177 <39151446+Vic177@users.noreply.github.com>
2018-08-11 17:05:58 +0800

    修改模型，添加认证

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -9,11 +11,25 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
         
-class User(db.Model):
+class User(UerMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+        
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+        
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+        
     
     def __repr__(self):
         return '<User %r>' %self.name
